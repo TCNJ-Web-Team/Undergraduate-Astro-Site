ef8a4de21e5d538852f4aba053a5534212daef1b
lvl12sealclubber <adam.holsten@gmail.com>
2023-11-01 11:55:55 -0400

    starting astro work

---
+++ src/data/wordpress.ts
@@ -0,0 +1,46 @@
+// interface WPGraphQLParams {
+//   query: string;
+//   variables?: object;
+// }
+
+// export async function wpquery({ query, variables = {} }: WPGraphQLParams) {
+//   const res = await fetch("http://undergraduate.local/graphql", {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ query, variables }),
+//   });
+//   if (!res.ok) {
+//     console.error(res);
+//     return {};
+//   }
+//   const { data } = await res.json();
+// }
+
+interface WPGraphQLParams {
+  query: string;
+  variables?: object;
+}
+
+export async function wpquery({ query, variables = {} }: WPGraphQLParams) {
+  const res = await fetch("http://undergraduate.local/graphql", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  if (!res.ok) {
+    console.error(res);
+    return {};
+  }
+
+  const { data } = await res.json();
+
+  return data;
+}
