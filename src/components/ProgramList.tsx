fc0b47ee5e34c362c3a8112469374284e0e72c55
lvl12sealclubber <adam.holsten@gmail.com>
2023-11-01 14:17:49 -0400

    update

---
+++ src/components/ProgramList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface MyComponentProps {
+  data: any;
+}
+// export default function ProgramList() {
+//   return (
+//     <div className="program-wrapper">
+//       <p>Test wrapper</p>
+//     </div>
+//   );
+// }
+
+const ProgramList: React.FC<MyComponentProps> = ({ data }) => {
+  // component implementation
+  // console.log(data.programs.nodes);
+  const programs = data.programs.nodes;
+  // console.log(programs);
+  return (
+    <div className="program-wrapper">
+      <p>Test</p>
+    </div>
+  );
+};
+export default ProgramList;
