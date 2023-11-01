67b2c29c66f570ff7c9ffa81059e193d18f5199c
lvl12sealclubber <adam.holsten@gmail.com>
2023-11-01 15:39:30 -0400

    working

--- src/components/ProgramList.jsx
+++ src/components/ProgramList.jsx
@@ -1,48 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { wpquery } from "../data/wordpress";
 
-export default function ProgramList() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const result = await wpquery({
-          query: `
-            query ProgramsListing {
-              programs {
-                nodes {
-                  program {
-                    degreeType
-                    slug
-                    school
-                    programOptions
-                    additionalOptions
-                    programAtAGlance
-                    learnMoreList {
-                      title
-                      slug
-                    }
-                  }
-                  title
-                }
-              }
-            }
-          `,
-        });
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-
-    fetchData();
-  }, []);
+export default function ProgramList({ data }) {
+  const [newData, setData] = useState(data);
 
   return (
     <div className="program-wrapper">
-      {data &&
-        data.programs.nodes.map((program, index) => {
+      {newData &&
+        newData.programs.nodes.map((program, index) => {
           const {
             degreeType,
             slug,
