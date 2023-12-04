3d5509db711253c69f45add53680da59977ef726
lvl12sealclubber <adam.holsten@gmail.com>
2023-12-04 10:09:19 -0500

    added custom fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bitter: ["Bitter", ...defaultTheme.fontFamily.sans],
+        chunkfive: ["Chunk Five", ...defaultTheme.fontFamily.sans],
+        opensans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        "custom-16.25": "4.063rem",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      // TEXT AND BG
+      tcnjyellow: "#FDD700",
+      tcnjblue: "#464E7D",
+      // BG COLORS
+      lightgrey: "#F1F1F1",
+      darkgrey: "#C8DAE6",
+      // TEXT COLORS
+      darkgrey: "#2E2E2E",
+      primarylinkblue: "#33739F",
+      darklinkblue: "#2B6388",
+      lightlinkblue: "#9BD0F3",
+    },
+  },
+  plugins: [],
+};
