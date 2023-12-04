073fa288fd4a3362ac1f19555ab12bb2cabe51a7
lvl12sealclubber <adam.holsten@gmail.com>
2023-12-04 13:42:35 -0500

    working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,20 +14,20 @@ export default {
       fontSize: {
         "custom-16.25": "4.063rem",
       },
-    },
-    colors: {
-      transparent: "transparent",
-      // TEXT AND BG
-      tcnjyellow: "#FDD700",
-      tcnjblue: "#464E7D",
-      // BG COLORS
-      lightgrey: "#F1F1F1",
-      darkgrey: "#C8DAE6",
-      // TEXT COLORS
-      darkgrey: "#2E2E2E",
-      primarylinkblue: "#33739F",
-      darklinkblue: "#2B6388",
-      lightlinkblue: "#9BD0F3",
+      colors: {
+        transparent: "transparent",
+        // TEXT AND BG
+        tcnjyellow: "#FDD700",
+        tcnjblue: "#464E7D",
+        // BG COLORS
+        lightgrey: "#F1F1F1",
+        darkgrey: "#C8DAE6",
+        // TEXT COLORS
+        darkgrey: "#2E2E2E",
+        primarylinkblue: "#33739F",
+        darklinkblue: "#2B6388",
+        lightlinkblue: "#9BD0F3",
+      },
     },
   },
   plugins: [],
