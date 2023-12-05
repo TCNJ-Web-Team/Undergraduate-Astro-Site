4e3f2fbfe0278e23fc6c483185b93e35384eea34
lvl12sealclubber <adam.holsten@gmail.com>
2023-12-05 10:41:53 -0500

    accordion working

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from "react";
+import "../styles/accordion-styles.scss";
+
+export default function Accodion({ children, header, idName, startOpen }) {
+  const [open, setOpen] = useState(startOpen);
+  const headerRef = useRef(null);
+
+  // useEffect(() => {
+  //   if (scholarship && headerRef.current) {
+  //     const headerElement = headerRef.current;
+  //     const words = headerElement.textContent.split(" ");
+  //     headerElement.innerHTML = `<span class="first-word">${words.shift()}</span> ${words.join(
+  //       " "
+  //     )}`;
+  //   }
+  // }, [scholarship]);
+  const onAccordionClick = () => {
+    // console.log(event);
+    setOpen(!open);
+  };
+
+  return (
+    <div className="accordion" id={idName}>
+      <h2
+        className={`${
+          open ? "accordion-header start-open active" : "accordion-header"
+        } font-bold text-[1.438rem]`}
+        onClick={onAccordionClick}
+      >
+        <span ref={headerRef}>{header}</span>
+        <span className="arrow">
+          {open ? (
+            <img
+              src="https://tcnj.edu/custom/icon-library/minus-dark.svg"
+              alt="close"
+              style={{ marginBottom: "5px" }}
+            />
+          ) : (
+            <img
+              src="https://tcnj.edu/custom/icon-library/plus-dark.svg"
+              alt="open"
+            />
+          )}
+        </span>
+      </h2>
+      <div
+        className={`${
+          open ? "accordion-content start-open active" : "accordion-content"
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
