8a4c8fe50062bd5373185a51745115dd0a5254a1
lvl12sealclubber <adam.holsten@gmail.com>
2024-02-09 11:42:10 -0500

    prog img

--- src/components/ProgramGlanceCopy.jsx
+++ src/components/ProgramGlanceCopy.jsx
@@ -2,9 +2,12 @@ export default function ProgramGlanceCopy({ copy, id }) {
   return (
     <div
       id={id}
-      className={`text-[20px]
+      className={`
+      text-[20px]
           leading-[35px]
           text-center
+          sm:text-[23px]
+          sm:leading-[45px]
           sm:text-left
           md:text-[1.75rem]
           ${id === "careers-body" ? "pb-[50px]" : ""}  
