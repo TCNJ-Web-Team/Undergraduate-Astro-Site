8967ea2339481fa7de6597ce24d8feb53bf06f05
lvl12sealclubber <adam.holsten@gmail.com>
2024-04-11 14:39:56 -0400

    updated navigation

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavigationBar() {
+  return (
+    <div
+      className="bg-[#F1F1F1] pt-[23px] pb-[23px] pl-[35px] pr-[35px]"
+      id="menu"
+    >
+      <p className="max-w-[1128px] mx-auto font-opensans text-[14px] leading-[34px] text-left uppercase">
+        Menu
+      </p>
+    </div>
+  );
+}
+
+export default NavigationBar;
--- src/components/TitleAndNav.jsx
+++ src/components/TitleAndNav.jsx
@@ -1,3 +1,5 @@
+import NavigationBar from "./NavigationBar";
+
 export default function TitleAndNav() {
   return (
     <>
@@ -13,14 +15,7 @@ export default function TitleAndNav() {
           Academics
         </p>
       </div>
-      <div
-        className="bg-[#F1F1F1] pt-[23px] pb-[23px] pl-[35px] pr-[35px]"
-        id="menu"
-      >
-        <p className="max-w-[1128px] mx-auto font-opensans text-[14px] leading-[34px] text-left uppercase">
-          Menu
-        </p>
-      </div>
+      <NavigationBar />
     </>
   );
 }
