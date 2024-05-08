0f22dd8fd02b603f3bc5f6cb8410a6f8a716a14a
lvl12sealclubber <adam.holsten@gmail.com>
2024-05-08 16:14:26 -0400

    updated animated title

--- src/components/program-page/AnimatedTitle.jsx
+++ src/components/program-page/AnimatedTitle.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 function AnimatedTitle({ title }) {
   return (
-    <motion.h1
+    <h1
       className="font-chunkfive
           text-tcnjblue
           text-[45px]
@@ -13,19 +13,42 @@ function AnimatedTitle({ title }) {
          
           md:text-custom-16.25
           md:leading-[4.688rem]"
-      initial={{ opacity: 0, y: 40 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{
-        // delay: 0.035,
-        ease: "easeOut",
-        duration: 0.45,
-        // type: "spring",
-        // stiffness: 50,
-      }}
     >
       {title}
-    </motion.h1>
+    </h1>
   );
 }
 
 export default AnimatedTitle;
+
+// import React from "react";
+// import { motion } from "framer-motion";
+
+// function AnimatedTitle({ title }) {
+//   return (
+//     <motion.h1
+//       className="font-chunkfive
+//           text-tcnjblue
+//           text-[45px]
+//           leading-[50px]
+//           sm:text-[60px]
+//           sm:leading-[60px]
+
+//           md:text-custom-16.25
+//           md:leading-[4.688rem]"
+//       initial={{ opacity: 0, y: 40 }}
+//       animate={{ opacity: 1, y: 0 }}
+//       transition={{
+//         // delay: 0.035,
+//         ease: "easeOut",
+//         duration: 0.45,
+//         // type: "spring",
+//         // stiffness: 50,
+//       }}
+//     >
+//       {title}
+//     </motion.h1>
+//   );
+// }
+
+// export default AnimatedTitle;
