a2dec3787d5e0f46ef38d2fd527d8f20778243af
lvl12sealclubber <adam.holsten@gmail.com>
2024-05-09 10:09:50 -0400

    update

--- src/components/QuoteContent.jsx
+++ src/components/QuoteContent.jsx
@@ -14,12 +14,18 @@ function QuoteContent({ content }) {
   const h2Content = h2Match ? h2Match[1] : "";
   const pContent = pMatch ? pMatch[1] : "";
 
-  //   console.log("H2 Content:", h2Content);
+  const headingLengthCheck =
+    h2Content.length > 100
+      ? "text-[18px] leading-[30px] md:text-[26px] md:leading-[45px]"
+      : "text-[26px] leading-[40px] md:text-[45px] md:leading-[65px]";
+  console.log("H2 Content length", h2Content.length);
   //   console.log("P Content:", pContent);
 
   return (
-    <div className="large-quote flex flex-col items-end">
-      <h2 className="font-alfaslab text-[45px] leading-[65px] text-tcnjblue">
+    <div className="large-quote flex flex-col items-end mb-[100px]">
+      <h2
+        className={`font-alfaslab ${headingLengthCheck} text-left sm:text-left text-tcnjblue`}
+      >
         {h2Content}
       </h2>
       <p className="font-domine text-[20px] leading-[35px]">{pContent}</p>
