dea8906d6afce8b1b346497038e32b5ed26d3047
lvl12sealclubber <adam.holsten@gmail.com>
2025-01-22 10:58:56 -0500

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,80 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NavigationBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div
-      className="bg-[#F1F1F1] pt-[23px] pb-[23px] pl-[35px] pr-[35px]"
-      id="menu"
-    >
-      <div className="max-w-[1128px] mx-auto flex justify-items-start items-center md:gap-[50px]">
-        <a
-          href="#program-at-a-glance-intro"
-          onClick={(e) => {
-            e.preventDefault();
-            document
-              .querySelector("#program-at-a-glance-intro")
-              .scrollIntoView({ behavior: "smooth" });
-          }}
-          className="font-opensans text-[14px] leading-[34px] text-left uppercase"
+    <div className="bg-gray-100 py-6 px-8" id="menu">
+      <div className="max-w-6xl mx-auto flex justify-items-start items-start md:gap-12 md:flex-row flex-col">
+        <button
+          onClick={toggleMenu}
+          className="md:hidden font-sans text-sm leading-9 uppercase"
         >
-          Overview
-        </a>
-        <a
-          href="#programs-of-study-container"
-          onClick={(e) => {
-            e.preventDefault();
-            document
-              .querySelector("#programs-of-study-container")
-              .scrollIntoView({ behavior: "smooth" });
-          }}
-          className="font-opensans text-[14px] leading-[34px] text-left uppercase"
-        >
-          Program Options
-        </a>
-        <a
-          href="#careers-intro"
-          onClick={(e) => {
-            e.preventDefault();
-            document
-              .querySelector("#careers-intro")
-              .scrollIntoView({ behavior: "smooth" });
-          }}
-          className="font-opensans text-[14px] leading-[34px] text-left uppercase"
-        >
-          Careers
-        </a>
-        <a
-          href="#additional-information"
-          onClick={(e) => {
-            e.preventDefault();
-            document
-              .querySelector("#additional-information")
-              .scrollIntoView({ behavior: "smooth" });
-          }}
-          className="font-opensans text-[14px] leading-[34px] text-left uppercase"
-        >
-          Additional Information
-        </a>
+          Menu
+        </button>
+
         <div
-          id="nav-button-container"
-          className="ml-auto flex gap-[10px] flex-row justify-items-start items-center"
+          className={`${isMenuOpen ? "flex" : "hidden"} md:flex flex-col md:flex-row md:gap-12  top-20 md:top-0 left-0  bg-gray-100 md:bg-transparent md:p-0 w-[100%] md:relative md:items-center`}
         >
           <a
-            href="https://admissions.tcnj.edu/apply/"
-            className="py-[12px] px-[25px] bg-tcnjyellow hover:bg-[#eabd00]
-            transition-all duration-300 ease-in-out
-            text-black font-opensans text-[14px] leading-[20px] uppercase font-bold"
+            href="#program-at-a-glance-intro"
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .querySelector("#program-at-a-glance-intro")
+                .scrollIntoView({ behavior: "smooth" });
+              setIsMenuOpen(false);
+            }}
+            className="font-sans text-sm leading-9 uppercase"
           >
-            Apply
+            Overview
           </a>
           <a
-            href="https://connect.tcnj.edu/register/prospect"
-            className="py-[12px] px-[25px] bg-tcnjyellow hover:bg-[#eabd00]
-            transition-all duration-300 ease-in-out
-            text-black font-opensans text-[14px] leading-[20px] uppercase font-bold"
+            href="#programs-of-study-container"
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .querySelector("#programs-of-study-container")
+                .scrollIntoView({ behavior: "smooth" });
+              setIsMenuOpen(false);
+            }}
+            className="font-sans text-sm leading-9 uppercase"
           >
-            Get Info
+            Program Options
           </a>
+          <a
+            href="#careers-intro"
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .querySelector("#careers-intro")
+                .scrollIntoView({ behavior: "smooth" });
+              setIsMenuOpen(false);
+            }}
+            className="font-sans text-sm leading-9 uppercase"
+          >
+            Careers
+          </a>
+          <a
+            href="#additional-information"
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .querySelector("#additional-information")
+                .scrollIntoView({ behavior: "smooth" });
+              setIsMenuOpen(false);
+            }}
+            className="font-sans text-sm leading-9 uppercase"
+          >
+            Additional Information
+          </a>
+          <div className="flex gap-2 flex-row justify-items-start items-center md:absolute md:right-0">
+            <a
+              href="https://admissions.tcnj.edu/apply/"
+              className="py-3 px-6 bg-yellow-400 hover:bg-yellow-500 transition-all duration-300 ease-in-out text-black font-sans text-sm leading-5 uppercase font-bold"
+            >
+              Apply
+            </a>
+            <a
+              href="https://connect.tcnj.edu/register/prospect"
+              className="py-3 px-6 bg-yellow-400 hover:bg-yellow-500 transition-all duration-300 ease-in-out text-black font-sans text-sm leading-5 uppercase font-bold"
+            >
+              Get Info
+            </a>
+          </div>
         </div>
       </div>
     </div>
