d62a18e98c4751f33d0476bf668e7595904d590b
lvl12sealclubber <adam.holsten@gmail.com>
2025-02-21 15:09:11 -0500

    new build

--- dist/_astro/AnimatedTitle.BKT4trbV.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime.C3nPk8XT.js";import"./index.Cq8P9Jm4.js";function p({title:n}){return t.jsx("h1",{className:`font-chunkfive
-          text-tcnjblue
-          text-[45px]
-          leading-[62px]
-          sm:text-[55px]
-          sm:leading-[75px]
-         sm:px-[45px] md:px-0
-          md:text-[65px]
-          md:leading-[89px]`,children:n})}export{p as default};
