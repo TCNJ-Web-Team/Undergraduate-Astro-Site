5465765b89cb74854b4d36b4d31f4d5be0333f06
lvl12sealclubber <adam.holsten@gmail.com>
2025-02-25 10:10:49 -0500

    new build

--- dist/_astro/AnimatedTitle.D3M_tYbI.js
+++ dist/_astro/AnimatedTitle.D3M_tYbI.js
@@ -1,9 +1,9 @@
 import{j as t}from"./jsx-runtime.C3nPk8XT.js";import"./index.Cq8P9Jm4.js";function p({title:n}){return t.jsx("h1",{className:`font-chunkfive
           text-tcnjblue
           text-[45px]
-          leading-[62px]
+          leading-[50px]
           sm:text-[55px]
-          sm:leading-[75px]
+          sm:leading-[60px]
          sm:px-[45px] md:px-0
           md:text-[65px]
-          md:leading-[89px]`,children:n})}export{p as default};
+          md:leading-[75px]`,children:n})}export{p as default};
