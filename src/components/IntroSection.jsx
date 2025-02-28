ef215d45bccaf91108df132e7d16665bfd81a684
lvl12sealclubber <adam.holsten@gmail.com>
2025-02-28 11:10:12 -0500

    Update IntroSection.jsx

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -57,9 +57,9 @@ export default function IntroSection() {
   return (
     <div
       id="intro"
-      className="w-[100%] h-[562px] relative 
+      className="w-[100%] h-[650px] relative 
       z-[1000]
-       border-[20px] sm:border-[35px] border-b-0 sm:border-b-0 border-tcnjyellow sm:h-[700px] md:h-[485px]"
+       border-[20px] sm:border-[35px] border-b-0 sm:border-b-0 border-tcnjyellow sm:h-[700px] md:h-[540px]"
     >
       <div className="max-w-[1128px] mx-auto relative z-10 text-white p-[35px] pl-[30px] pr-[30px] text-center sm:pt-[77px] md:pt-[67px] md:text-left lg:px-0">
         <div
@@ -69,7 +69,7 @@ export default function IntroSection() {
           <a>Home</a> / Explore Undergraduate Programs
         </div>
         <motion.h1
-          className="pt-[100px] font-alfaslab text-tcnjyellow text-[32px] leading-[38px] pb-[10px] sm:pt-[150px] sm:text-[55px] sm:leading-[55px] md:text-[60px] md:leading-[65px] md:pt-[85px]"
+          className="pt-[100px] font-alfaslab text-tcnjyellow text-[32px] leading-[38px] pb-[10px] sm:pt-[150px] sm:text-[40px] sm:leading-[45px] md:text-[60px] md:leading-[65px] md:pt-[85px]"
           initial={{ opacity: 0, y: -5 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
@@ -78,10 +78,10 @@ export default function IntroSection() {
             duration: 0.35,
           }}
         >
-          Explore Undergraduate Programs
+          160+ Undergraduate Programs
         </motion.h1>
         <motion.div
-          className="font-domine text-[20px] leading-[26px] sm:text-[25px] sm:leading-[35px] md:text-[23px] font-normal"
+          className="font-domine text-[18px] leading-[24px] sm:text-[20px] sm:leading-[27px] md:leading-[35px] md:text-[23px] font-normal"
           initial={{ opacity: 0, y: -5 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
@@ -181,7 +181,7 @@ export default function IntroSection() {
         <img
           src="https://tcnj.edu/custom/undergraduate/img/hero-mobile.jpg"
           alt="TCNJ scenery"
-          className="w-[100%] h-[542px] sm:h-[665px] md:h-[450px] object-cover"
+          className="w-[100%] h-[630px] sm:h-[665px] md:h-[505px] object-cover"
         />
       </picture>
       <div className="bg-black absolute w-[100%] h-[100%] top-0 left-0 z-[5] opacity-[.5] sm:opacity-[.25]"></div>
