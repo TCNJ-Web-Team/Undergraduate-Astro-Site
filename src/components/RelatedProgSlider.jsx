5acbd4c766c5db5e102d3eaa6e98a8a76676d15d
lvl12sealclubber <adam.holsten@gmail.com>
2025-03-03 11:01:58 -0500

    Update RelatedProgSlider.jsx

--- src/components/RelatedProgSlider.jsx
+++ src/components/RelatedProgSlider.jsx
@@ -11,7 +11,7 @@ import "../styles/custom-swiper.scss";
 export default function RelatedProgSlider({ learnMoreList }) {
   const [marginLeft, setMarginLeft] = useState(0);
   const containerRef = useRef(null);
-
+  console.log(learnMoreList);
   useEffect(() => {
     function updateMargin() {
       if (containerRef.current) {
@@ -65,28 +65,64 @@ export default function RelatedProgSlider({ learnMoreList }) {
               Related Programs
             </h2>
           </div>
-          {learnMoreList.map((content) => (
-            <SwiperSlide
-              className="border border-[#BFBFBF] w-auto"
-              id={content.title.replace(/\s+/g, "-").toLowerCase()}
-              key={content.title.replace(/\s+/g, "-").toLowerCase()}
-            >
-              <a
-                className="p-[35px] sm:py-[45px] sm:px-[25px] md:px-[50px] min-h-[250px] block"
-                href={content.slug}
+          {learnMoreList.map((content) => {
+            // Define a mapping of titles to slugs
+            const slugMapping = {
+              "Accelerated Second-Degree Bachelor of Science in Nursing":
+                "absn",
+              "Elementary Education of the Deaf and Hard of Hearing":
+                "/elementary-education-dhh",
+              "Inclusive Education — Early Childhood Education (P–3)":
+                "/inclusive-education-p-3",
+              "Inclusive Education — Elementary Education (K–6)":
+                "/inclusive-education-k-6",
+              "Kinesiology and Health Sciences": "/kinesiology-health-sciences",
+              "Mathematics and Statistics": "/mathematics",
+              "Photography and Video": "/photography-video",
+              Robotics: "/robotics-engineering-science",
+              "Spanish — World Languages and Linguistics": "/spanish-wll",
+              "Spanish and ESL/Bilingual Education": "/spanish-esl",
+              "Speech-Language Pathology and Audiology":
+                "/speech-pathology-audiology",
+              "Speech-Language Pathology": "/slp",
+              "Teaching English as a Second Language": "/tesl",
+              "Technology and Engineering Education":
+                "/technology-engineering-education",
+              "Women’s, Gender, and Sexuality Studies": "/wgss",
+              "World Languages and Linguistics": "/world-languages-linguistics",
+            };
+
+            // Assign the slug based on mapping or generate from title if slug is "#"
+            content.slug =
+              slugMapping[content.title] ||
+              (content.slug === "#"
+                ? content.title.replace(/\s+/g, "-").toLowerCase()
+                : content.slug);
+
+            // console.log(content.title);
+            return (
+              <SwiperSlide
+                className="border border-[#BFBFBF] w-auto"
+                id={content.title.replace(/\s+/g, "-").toLowerCase()}
+                key={content.title.replace(/\s+/g, "-").toLowerCase()}
               >
-                <p className="font-domine font-semibold text-[18px] leading-[28px] sm:leading-[21px] md:text-[21px] md:leading-[24px]">
-                  {content.title}
-                </p>
-                <hr className="m-[25px] mr-0 ml-0 md:mt-[27px]" />
-                {content.schoolOrAdditionalLabel && (
-                  <p className="font-opensans text-[14px] leading-[20px]">
-                    {content.schoolOrAdditionalLabel}
+                <a
+                  className="p-[35px] sm:py-[45px] sm:px-[25px] md:px-[50px] min-h-[250px] block"
+                  href={content.slug}
+                >
+                  <p className="font-domine font-semibold text-[18px] leading-[28px] sm:leading-[21px] md:text-[21px] md:leading-[24px]">
+                    {content.title}
                   </p>
-                )}
-              </a>
-            </SwiperSlide>
-          ))}{" "}
+                  <hr className="m-[25px] mr-0 ml-0 md:mt-[27px]" />
+                  {content.schoolOrAdditionalLabel && (
+                    <p className="font-opensans text-[14px] leading-[20px]">
+                      {content.schoolOrAdditionalLabel}
+                    </p>
+                  )}
+                </a>
+              </SwiperSlide>
+            );
+          })}
           <div
             id="swiper-navigation"
             className="lg:!right-0"
