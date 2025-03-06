0a914512884558ae54db13784fe5c616240b4250
lvl12sealclubber <adam.holsten@gmail.com>
2025-03-06 15:53:59 -0500

    update

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -66,7 +66,7 @@ export default function IntroSection() {
           id="breadcrumbs"
           className="font-domine text-[17px] leading-[20px] sm:text-[18px] sm:leading-[26px] sm:text-left"
         >
-          <a>TCNJ Home</a> /{" "}
+          <a href="https://tcnj.edu">TCNJ Home</a> /{" "}
           <span className="font-[600]">Explore Programs</span>
         </div>
 
