557bb2a932002ea1d3df62fbf6b6cae55e6375b8
lvl12sealclubber <adam.holsten@gmail.com>
2025-03-14 13:10:34 -0400

    Update ProgramCard.jsx

--- src/components/ProgramCard.jsx
+++ src/components/ProgramCard.jsx
@@ -143,6 +143,7 @@ const ProgramCard = ({ program, listType, index, animationState }) => {
                     }`}
                   >
                     <img
+                      loading="lazy"
                       src={`/card-icons/${formatOption(option)}.svg`}
                       alt={option}
                       className={`${getImageDimensions(option)} object-contain`}
