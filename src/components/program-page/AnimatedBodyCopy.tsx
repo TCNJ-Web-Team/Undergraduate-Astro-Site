6a0979a0224993430c80e226862fb6701f411c0f
lvl12sealclubber <adam.holsten@gmail.com>
2025-03-15 15:35:53 -0400

    update

--- src/components/program-page/AnimatedBodyCopy.tsx
+++ src/components/program-page/AnimatedBodyCopy.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProgramGlanceCopy from "../ProgramGlanceCopy";
 import ProgramCareersHeading from "../ProgramCareersHeading";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { fadeInAnimationVariant } from "../../animations/fadeInVariant";
 
 type Props = {
@@ -15,24 +15,18 @@ const AnimatedBodyCopy: React.FC<Props> = ({ left, right, body, heading }) => {
   const headingLower = heading.replace(/\s+/g, "-").toLowerCase();
 
   return (
-    <motion.div
+    <div
       id={headingLower}
       className={`relative sm:px-[45px] md:pl-[20px] md:pr-[20px] lgMd:pl-0  lg:pl-[0px] lg:pr-[0px]`}
-      variants={fadeInAnimationVariant}
-      initial="initial"
-      whileInView="animate"
-      viewport={{ once: true }}
     >
-      <motion.h2
+      <h2
         className={`text-center font-chunkfive text-tcnjblue text-[45px] leading-[62px] pb-[25px] sm:pb-[20px] sm:text-[55px] sm:leading-[75px] sm:text-left md:text-[65px] md:leading-[89px] md:pb-[23px]
           lg:pb-[14px]
         
         `}
-        variants={fadeInAnimationVariant}
-        custom={1}
       >
         {heading}
-      </motion.h2>
+      </h2>
 
       {body && <ProgramGlanceCopy id={`${headingLower}-body`} copy={body} />}
       <div
@@ -54,24 +48,20 @@ const AnimatedBodyCopy: React.FC<Props> = ({ left, right, body, heading }) => {
           ${left ? "sm:pt-[70px] md:pt-[89px] lg:pt-[85px]" : "pt-0"}`}
         >
           {left && (
-            <motion.div
+            <div
               id={`${headingLower}-left`}
               className="md:max-w-[45%] lg:max-w-[50%]"
               dangerouslySetInnerHTML={{ __html: left }}
-              variants={fadeInAnimationVariant}
-              custom={3}
             />
           )}
 
-          <motion.div
+          <div
             id={`${headingLower}-right`}
             dangerouslySetInnerHTML={{ __html: right || "" }}
-            variants={fadeInAnimationVariant}
-            custom={4}
           />
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
--- src/components/program-page/AnimatedProgramAtAGlance.tsx
+++ src/components/program-page/AnimatedProgramAtAGlance.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { fadeInAnimationVariant } from "../../animations/fadeInVariant";
+// import { motion } from "framer-motion";
+// import { fadeInAnimationVariant } from "../../animations/fadeInVariant";
 
 interface AnimatedProgramProps {
   programAtAGlance: string;
@@ -37,7 +37,48 @@ const AnimatedProgramAtAGlance: React.FC<AnimatedProgramProps> = ({
         className="pt-[91px] sm:pt-[103px]
 md:pt-[135px] lg:pt-[187px]"
       ></div>
-      <motion.div
+      <div
+        id="program-at-a-glance"
+        className={`text-left ${programImageCheck ? "pb-[145px]  sm:pb-[160px] md:pb-[150px] lg:pb-[200px]" : "pb-[100px]  sm:pb-[115px] md:pb-[150px] lg:pb-[200px]"}   `}
+      >
+        <h2
+          className={`text-center font-chunkfive text-tcnjblue text-[45px] leading-[62px] pb-[20px] sm:pb-[27px] sm:text-[55px] sm:leading-[75px] sm:text-left md:text-[65px] md:leading-[89px] md:pb-[23px]
+            lg:pb-[14px]
+        
+        `}
+        >
+          At a Glance
+        </h2>
+
+        <div
+          id="program-glance"
+          className={`text-[20px] leading-[35px] text-center sm:text-left sm:text-[21px] sm:leading-[40px] md:text-[23px] md:leading-[45px] yellow-body-copy`}
+          dangerouslySetInnerHTML={{ __html: programAtAGlance }}
+        />
+
+        <div className="flex flex-col gap-[20px] sm:flex-row sm:mt-[64px] sm:gap-[28px] md:gap-5 md:flex-row mt-[50px]">
+          <a
+            href="https://admissions.tcnj.edu/apply/"
+            target="_blank"
+            className="program-button-glance-gtm border-2 border-tcnjblue p-4 pt-[36px] pb-[36px] sm:w-[100%] text-center font-opensans uppercase text-tcnjblue font-semibold text-base md:max-w-[300px]
+                hover:bg-tcnjblue hover:text-white transition-all
+                "
+          >
+            Apply
+          </a>
+          <a
+            href="https://connect.tcnj.edu/register/prospect"
+            target="_blank"
+            className="border-2 border-tcnjblue p-4 pt-[36px] pb-[36px] sm:w-[100%] text-center font-opensans uppercase text-tcnjblue font-semibold text-base md:max-w-[300px]
+                hover:bg-tcnjblue hover:text-white
+                transition-all
+                "
+          >
+            Request Info
+          </a>
+        </div>
+      </div>
+      {/* <motion.div
         id="program-at-a-glance"
         className={`text-left ${programImageCheck ? "pb-[145px]  sm:pb-[160px] md:pb-[150px] lg:pb-[200px]" : "pb-[100px]  sm:pb-[115px] md:pb-[150px] lg:pb-[200px]"}   `}
         variants={fadeInAnimationVariant}
@@ -89,7 +130,7 @@ md:pt-[135px] lg:pt-[187px]"
             Request Info
           </a>
         </motion.div>
-      </motion.div>
+      </motion.div> */}
     </>
   );
 };
